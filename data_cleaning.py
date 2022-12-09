39b5f38e69cb87724df0f3700b48522f164d19cd
Alex Liu <alexliux@uchicago.edu>
2022-12-09 09:28:36 -0800

    update clean data

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,4 +12,25 @@ data["YEAR"] = data["SOLD DATE"].dt.year
 # get year and month 
 data["YEAR_MONTH"] = data["SOLD DATE"].dt.to_period('M').astype(str)
 
+#categorize propoerty types
+data['PROPERTY TYPE RAW'] = data['PROPERTY TYPE']
+def categorise(row):  
+    if row['PROPERTY TYPE RAW']=='Single Family Residential':
+        return 'Single Family Residential'
+    elif row['PROPERTY TYPE RAW']=='Multi-Family (2-4 Unit)' or row['PROPERTY TYPE RAW']=='Multi-Family (5+ Unit)':
+        return 'Multi-Family Residential'
+    elif row['PROPERTY TYPE RAW']=='Condo/Co-op' or row['PROPERTY TYPE RAW']=='Co-op':
+        return 'Condo/Co-op'
+    elif row['PROPERTY TYPE RAW']=='Townhouse':
+        return 'Townhouse'
+    elif row['PROPERTY TYPE RAW']=='Mobile/Manufactured Home' or row['PROPERTY TYPE RAW']=='Ranch' or row['PROPERTY TYPE RAW']=='Timeshare' or row['PROPERTY TYPE RAW']=='Parking':
+        return 'Business/Industry'
+    elif row['PROPERTY TYPE RAW']=='Other' or row['PROPERTY TYPE RAW']=='Vacant Land':
+        return 'Land/Other'
+    elif row['PROPERTY TYPE RAW']=='Unknown':
+        return 'Unknown'
+    return 
+
+data['PROPERTY TYPE'] = data.apply(lambda row: categorise(row), axis=1)
+
 data.to_csv("data/redfin-sold-last-five-years/all_cleaned.csv")
